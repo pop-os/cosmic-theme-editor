3819d7d1a720c7799f6eb6e423cd0ee5b8c65797
Ashley Wulber <Ashley@system76.com>
2022-03-15 16:23:21 -0400

    fix: styling border radius

--- src/model/derivation.rs
+++ src/model/derivation.rs
@@ -47,12 +47,18 @@ impl AsCss for Container {
             ContainerType::Primary => "primary-container",
             ContainerType::Secondary => "secondary-container",
         };
+        let top_level_border_radius = if self.prefix != ContainerType::Background {
+            "border-radius: 8px;"
+        } else {
+            ""
+        };
+
         format!(
             r#"/* {prefix_lower} CSS */
 *.{prefix_lower} {{
   background-color: {container};
   color: {container_text};
-  border-radius: 8px;
+  {top_level_border_radius}
 }}
 
 *.{prefix_lower}-component {{
